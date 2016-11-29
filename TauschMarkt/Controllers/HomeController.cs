81d180ca10020db39246153b865bab99e2e912c1
Adrian <adrian.roesser@yahoo.de>
2016-11-29 14:42:47 +0100

    endlich ois gleich

--- TauschMarkt/Controllers/HomeController.cs
+++ TauschMarkt/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TauschMarkt.Models;
 
 namespace TauschMarkt.Controllers
 {
@@ -13,11 +17,53 @@ namespace TauschMarkt.Controllers
             return View();
         }
 
-        public ActionResult ShopItem()
+        public ActionResult ShopItem(int id)
         {
             ViewBag.Message = "Your contact page.";
+            MySqlConnection connection = new MySqlConnection("server=localhost;database=tauschmarkt;uid=root;password=");
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = $"SELECT Name, Preis, kategroie_id, status, beschreibung FROM artikel WHERE id = {id}";
+            var reader = command.ExecuteReader();
+            Artikel art = new Artikel();
+            if (reader.Read())
+            {
+                art.Name = reader["Name"].ToString();
+                art.Preis = reader["Preis"].ToString();
+                art.beschreibung = reader["beschreibung"].ToString();
 
-            return View();
+            }
+            return View(art);
+        }
+
+        public ActionResult ProductPicture(int id)
+        {
+            MySqlConnection connection = new MySqlConnection("server=localhost;database=tauschmarkt;uid=root;password=");
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = $"SELECT picture FROM artikel WHERE id = {id}";
+
+            var reader = command.ExecuteReader();
+            byte[] picString = new byte[] { };
+            if (reader.Read())
+            {
+                picString = (byte[])reader["picture"];
+            }
+            connection.Close();
+            Response.AppendHeader("Content-Type", "image/jpeg");
+
+
+            return File(picString, "image/jpeg");
+        }
+
+        public Image Base64ToImage(byte[] imageBytes)
+        {
+            //byte[] imageBytes = Convert.FromBase64String(base64string);
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = Image.FromStream(ms, true);
+                return image;
+            }
         }
 
         public ActionResult About()
---
+++ TauschMarkt/Models/Artikel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TauschMarkt.Models
+{
+    public class Artikel
+    {
+
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Preis { get; set; }
+        public int kategroie_id { get; set; }
+        public int status { get; set; }
+        public string beschreibung { get; set; }
+    }
+}
