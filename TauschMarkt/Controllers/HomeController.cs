1308db5d24d3550d56ae933e2beb957392c71959
Adrian <adrian.roesser@yahoo.de>
2016-12-06 13:05:13 +0100

    ProductController erstellt und so

--- TauschMarkt/Controllers/HomeController.cs
+++ TauschMarkt/Controllers/HomeController.cs
@@ -37,24 +37,6 @@ namespace TauschMarkt.Controllers
             return View(lohl);
         }
 
-        public ActionResult ShopItem(int id)
-        {
-            ViewBag.Message = "Your contact page.";
-            MySqlConnection connection = new MySqlConnection("server=localhost;database=tauschmarkt;uid=root;password=");
-            connection.Open();
-            MySqlCommand command = connection.CreateCommand();
-            command.CommandText = $"SELECT Name, Preis, kategroie_id, status, beschreibung FROM artikel WHERE id = {id}";
-            var reader = command.ExecuteReader();
-            Artikel art = new Artikel();
-            if (reader.Read())
-            {
-                art.Name = reader["Name"].ToString();
-                art.Preis = reader["Preis"].ToString();
-                art.beschreibung = reader["beschreibung"].ToString();
-
-            }
-            return View(art);
-        }
 
         public ActionResult ProductPicture(int id)
         {
---
+++ TauschMarkt/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TauschMarkt.Models;
+
+namespace TauschMarkt.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShopItem(int id)
+        {
+            ViewBag.Message = "Your contact page.";
+            MySqlConnection connection = new MySqlConnection("server=localhost;database=tauschmarkt;uid=root;password=");
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = $"SELECT Name, Preis, kategroie_id, status, beschreibung FROM artikel WHERE id = {id}";
+            var reader = command.ExecuteReader();
+            Artikel art = new Artikel();
+            if (reader.Read())
+            {
+                art.Name = reader["Name"].ToString();
+                art.Preis = reader["Preis"].ToString();
+                art.beschreibung = reader["beschreibung"].ToString();
+
+            }
+            return View(art);
+        }
+    }
+}
